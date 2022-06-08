185d3075132b4267f1cae239aa9df29b0f60027f
13berkecan <13berkecan@gmail.com>
2022-06-08 22:25:13 +0300

    web files

---
+++ wwwroot/js/scripts.js
@@ -0,0 +1,9 @@
+function getDateTime () {
+    let datentime = new Date();
+    let date = datentime.toLocaleDateString("en-US", {weekday: "short", month: "short", day: "numeric" });
+    let time = datentime.toLocaleTimeString("en-Us", {hour12: false, hour: "2-digit", minute: "2-digit"})
+    document.getElementById("date").innerHTML=date;
+    document.getElementById("time").innerHTML=time;
+
+    setTimeout(getDateTime, 1000);
+}
